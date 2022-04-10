64f35f5b1eb0968347aeced23c6ad9fe945a52e2
unknown <kikridu@gmail.com>
2022-04-10 15:40:52 -0700

    post and get stuff done

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path')
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals.json')
+const { animals } = require('./data/animals')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json())
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -35,6 +41,33 @@ function findById(id, animalsArray) {
     return result
 }
 
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    animalsArray.push(animal)
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal
+}
+
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals
     if (req.query) {
@@ -45,8 +78,24 @@ app.get('/api/animals', (req, res) => {
 
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
-    res.json(result);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404)
+    }
 })
+
+app.post('/api/animals', (req, res) => {
+    req.body.id = animals.length.toString();
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.')
+    }
+
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
